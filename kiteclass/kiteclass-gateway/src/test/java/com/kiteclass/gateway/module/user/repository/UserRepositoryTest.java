ecb14986c1c3702f71563bf2a6d6f5bde59b3d93
VictorAurelius <vankiet14491@gmail.com>
2026-01-30 14:46:19 +0700

    uncheck: update prompt

--- kiteclass/kiteclass-gateway/src/test/java/com/kiteclass/gateway/module/user/repository/UserRepositoryTest.java
+++ kiteclass/kiteclass-gateway/src/test/java/com/kiteclass/gateway/module/user/repository/UserRepositoryTest.java
@@ -11,7 +11,6 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import reactor.test.StepVerifier;
 
 import static org.assertj.core.api.Assertions.assertThat;
