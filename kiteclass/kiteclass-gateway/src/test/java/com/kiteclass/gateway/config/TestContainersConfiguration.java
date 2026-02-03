cd90902bd4a610140e3478baa6e0eefeb7ac418e
VictorAurelius <vankiet14491@gmail.com>
2026-02-03 08:44:04 +0700

    uncheck: update prompt

--- kiteclass/kiteclass-gateway/src/test/java/com/kiteclass/gateway/config/TestContainersConfiguration.java
+++ kiteclass/kiteclass-gateway/src/test/java/com/kiteclass/gateway/config/TestContainersConfiguration.java
@@ -29,7 +29,7 @@ public class TestContainersConfiguration {
      */
     @Bean
     @ServiceConnection
-    public PostgreSQLContainer<?> postgresContainer() {
+    PostgreSQLContainer<?> postgresContainer() {
         return new PostgreSQLContainer<>(DockerImageName.parse("postgres:15-alpine"))
                 .withDatabaseName("testdb")
                 .withUsername("test")
