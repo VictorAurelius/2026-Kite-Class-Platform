b9d593e6156a55dda1b707f8b75910f8228c4f6a
VictorAurelius <vankiet14491@gmail.com>
2026-02-10 04:49:59 +0000

    chore: remove unused import from ProfileFetcherTest
    Changes:
    - Remove unused anyString import after HMAC auth refactor
    - Update conversation log

--- kiteclass/kiteclass-gateway/src/test/java/com/kiteclass/gateway/service/ProfileFetcherTest.java
+++ kiteclass/kiteclass-gateway/src/test/java/com/kiteclass/gateway/service/ProfileFetcherTest.java
@@ -21,7 +21,6 @@ import java.util.HashMap;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.never;
