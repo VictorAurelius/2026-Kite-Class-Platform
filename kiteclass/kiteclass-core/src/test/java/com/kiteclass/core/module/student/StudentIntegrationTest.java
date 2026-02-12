5dcbc61742e52e566fdb0ecaf294f9118a11688c
VictorAurelius <vankiet14491@gmail.com>
2026-02-12 04:49:20 +0000

    test(core): activate test profile for StudentIntegrationTest
    Changes:
    - Add @ActiveProfiles("test") to enable TestSecurityConfig
    - Import ActiveProfiles annotation
    
    Fixes 403 Forbidden errors by ensuring Spring Security is disabled
    in integration tests via TestSecurityConfig profile activation.

--- kiteclass/kiteclass-core/src/test/java/com/kiteclass/core/module/student/StudentIntegrationTest.java
+++ kiteclass/kiteclass-core/src/test/java/com/kiteclass/core/module/student/StudentIntegrationTest.java
@@ -16,6 +16,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -49,6 +50,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @Import({TestContainersConfiguration.class, TestSecurityConfig.class, TestTenantContextFilter.class})
 @ContextConfiguration(initializers = TestContainersConfiguration.Initializer.class)
 @Transactional
