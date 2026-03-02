704f490036658a2b1daafc84018b222c781b334a
VictorAurelius <vankiet14491@gmail.com>
2026-03-02 03:44:08 +0000

    fix(payment): remove wildcard imports

--- kiteclass/kiteclass-core/src/main/java/com/kiteclass/core/module/payment/entity/PaymentWebhookLog.java
+++ kiteclass/kiteclass-core/src/main/java/com/kiteclass/core/module/payment/entity/PaymentWebhookLog.java
@@ -1,8 +1,18 @@
 package com.kiteclass.core.module.payment.entity;
 
 import io.hypersistence.utils.hibernate.type.json.JsonBinaryType;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.Type;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
