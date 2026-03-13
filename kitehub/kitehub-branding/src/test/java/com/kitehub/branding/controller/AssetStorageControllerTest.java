7ce4f9f70f01bdde4a476b574b7e45c29bb7e8ee
Vann Kite <146878611+VictorAurelius@users.noreply.github.com>
2026-03-13 09:28:32 +0700

    fix(tests): Add missing Map import
    1-line fix for missing java.util.Map import ✅

--- kitehub/kitehub-branding/src/test/java/com/kitehub/branding/controller/AssetStorageControllerTest.java
+++ kitehub/kitehub-branding/src/test/java/com/kitehub/branding/controller/AssetStorageControllerTest.java
@@ -21,6 +21,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
